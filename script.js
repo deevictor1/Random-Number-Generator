954aa056a96bcd391c37f407cca7c2434cc71aa4
Victor <vicomsms@gmail.com>
2022-04-01 15:46:47 +0100

    Initial Commit

--- script.js
+++ script.js
@@ -1,52 +1,20 @@
-// We Will Do This Using Two Methods
+const number = document.querySelector(".number");
+const generateNumber = document.querySelector(".generate");
 
-// Method One - Using Multiple Event Listeners
-const count = document.querySelector(".count");
-const add = document.querySelector (".add");
-const subtract = document.querySelector (".subtract");
-const resetCount = document.querySelector (".reset");
 
-add.addEventListener ("click", () => {
-    count.innerHTML++;
-        setColor();
+generateNumber.addEventListener ("click", ()=>{
+    const randomNumber = Math.floor(Math.random() * 100);
+    number.innerHTML = randomNumber;
 });
-subtract.addEventListener ("click", () => {
-    count.innerHTML--;
-        setColor();
-});
-resetCount.addEventListener ("click", () => {
-    count.innerHTML = 0;
-        setColor();
-});
+const randomNumber = Math.floor(Math.random() * 100);
+    number.innerHTML = randomNumber;
 
 
-// Method Two - Using Event Delegation
-// const buttons = document.querySelector(".buttons");
-// const count = document.querySelector(".count");
-
-// buttons.addEventListener ("click", (e) => {
-//     if (e.target.classList.contains ("add")) {
-//         count.innerHTML++;
-//         setColor();
-//     }
-//     if (e.target.classList.contains ("subtract")) {
-//         count.innerHTML--;
-//         setColor();
-//     }
-//     if (e.target.classList.contains ("reset")) {
-//         count.innerHTML = 0;
-//         setColor();
-//     }
-// });
 
 
-// Changing Count Number Colour 
-function setColor() {
-    if (count.innerHTML > 0) {
-        count.style.color = "yellow";
-    } else if (count.innerHTML < 0){
-        count.style.color = "orangered";
-    } else {
-        count.style.color = "white";
-    }
-}
+
+
+
+
+// const rand = Math.floor(Math.random() * 100);
+//     console.log(rand);
